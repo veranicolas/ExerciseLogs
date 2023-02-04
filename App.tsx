763f246261f4714d88c3a281f947a7bccdadb07d
Nicolas Vera <nicolasveraa97@gmail.com>
2023-02-04 03:28:38 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function App(): JSX.Element {
+
+  return (
+    <SafeAreaView>
+      
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
---
+++ README.md
@@ -0,0 +1,1 @@
+# GymLogs
