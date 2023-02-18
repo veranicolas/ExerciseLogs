22280ac8e7cfae64810b14c7dd219a3514baf835
Nicolas Vera <nicolasveraa97@gmail.com>
2023-02-18 18:39:49 -0300

    Add inputs error visual changes

--- src/pages/ExerciseForm/ExerciseForm.tsx
+++ src/pages/ExerciseForm/ExerciseForm.tsx
@@ -1,28 +1,69 @@
 import React, {useState} from 'react'
-import { View, NativeModules, TextInput, Dimensions } from 'react-native'
+import { View, NativeModules, TextInput, Dimensions, Text, StyleSheet } from 'react-native'
+import { useForm, Controller } from 'react-hook-form'
 
 import { CustomButton } from '../../components/CustomButton'
 
 const ExerciseForm = () =>{
 
-    const [exercise, setExercise] = useState('')
+    const { control, handleSubmit, formState: { errors } } = useForm({
+        defaultValues: {
+          exercise: '',
+          type: '',
+          amount:0,
+          reps:0
+        }
+      });
   
     const locale = NativeModules.I18nManager.localeIdentifier // Add this line in the beggining to set the global language of the app
   
-    const onHandleSubmit = () =>{
-      console.log('Data submitted', {exercise})
+    const onHandleSubmit = (data:any) =>{
+      console.log('Data submitted', data)
     }
   
     return(
-      <View style={{padding:20, height:Dimensions.get('screen').height * 0.73, justifyContent:'space-between'}}>
-        <TextInput
-          placeholder='Exercise name'
-          style={{borderColor:'grey', borderWidth:1, borderRadius:8}}
-          onChangeText={(value)=> setExercise(value)}
+      <View style={{padding:20, height:Dimensions.get('screen').height * 0.73, justifyContent:'flex-start'}}>
+        <Controller
+            control={control}
+            rules={{
+                required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+                placeholder='Exercise'
+                style={errors.exercise ? styles.inputErrors : styles.input}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+            />
+            )}
+            name="exercise"
         />
-        <CustomButton title="Submit" handlePress={onHandleSubmit}/>
+        <Controller
+            control={control}
+            rules={{
+                required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+                style={errors.type ? styles.inputErrors : styles.input}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                placeholder='Type of exercise'
+            />
+            )}
+            name="type"
+        />
+    
+        <CustomButton title="Submit" handlePress={handleSubmit(onHandleSubmit)}/>
       </View>
     )
 }
+
+const styles = StyleSheet.create({
+    input:{borderColor:'grey', borderWidth:1, borderRadius:8, marginVertical:10},
+    inputErrors:{borderColor:'red', borderWidth:1, borderRadius:8, marginVertical:10},
+})
 
 export { ExerciseForm }
