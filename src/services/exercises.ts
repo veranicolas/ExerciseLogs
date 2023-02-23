068012c6ca341c55efbda7f35f128ad624010151
Nicolas Vera <nicolasveraa97@gmail.com>
2023-02-23 13:45:25 -0300

    Add configuration for .env and services folder

---
+++ src/services/exercises.ts
@@ -0,0 +1,13 @@
+import { API_URL } from '@env'
+
+const deleteEntryFromUser = () =>{
+
+    return {}
+}
+
+const getAllExercisesFromUser = () =>{
+
+    return {}
+}
+
+export { getAllExercisesFromUser }
